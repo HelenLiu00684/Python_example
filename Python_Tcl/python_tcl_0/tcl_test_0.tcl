c35ca689fc41100ee59f152e8d31c9bc1b227ff3
Helen Liu <qjliuehelen@gmail.com>
2025-10-29 07:38:36 -0400

    Tcl invoke a file to execute some commands

--- Python_Tcl/python_tcl_0/tcl_test_0.tcl
+++ Python_Tcl/python_tcl_0/tcl_test_0.tcl
@@ -1,11 +1,47 @@
 # test.tcl
-# Open file in append mode ("a")
-# set fp [open "tcl_0_result.txt" "a"]
 
-puts "Running TCL script..."
+set scriptDir [file dirname [info script]]
+set filePath [file join $scriptDir "tcl_commands.txt"]
 
-set result [exec uname -a]
-puts "System info 'uname -': $result"
+#puts "Script directory: $scriptDir"
+#puts "Target file path: $filePath"
 
-set result [exec cat /proc/stat]
-puts "CPU stats 'cat /proc/stat':\n$result"
+if {![file exists $filePath]} {
+    puts "❌ File not found: $filePath"
+    exit 1
+}
+
+
+puts "Open TCL file..."
+set fp [open $filePath r]
+set content [split [string map {"\r" ""} [read $fp]] "\n"]
+close $fp
+
+#puts "Running TCL script list:\n"
+#foreach line $content {
+#    puts "Command line: $line"
+#}
+
+puts "\nRunning system commands..."
+foreach line $content {
+    if {$line ne ""} {
+        catch {
+            #set result [eval exec $line]
+            #puts "Command: $line"
+            #puts "Result:\n$result\n"
+            set result [exec $line]
+            puts "Line: $line \n': $result"
+
+        } errMsg
+        if {[info exists errMsg] && $errMsg ne ""} {
+            puts "⚠️ Error executing '$line': $errMsg\n"
+        }
+    }
+}
+
+puts "✅ Tcl finished successfully."
+
+
+
+
+
