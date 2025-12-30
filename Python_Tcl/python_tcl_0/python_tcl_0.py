c8edad5a00abf3095af26829ff08d57bf66279b7
HelenLiu <your_email@example.com>
2025-12-30 16:46:41 -0500

    Add Python Tcl and threading examples

--- Python_Tcl/python_tcl_0/python_tcl_0.py
+++ Python_Tcl/python_tcl_0/python_tcl_0.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import sys
+import psutil 
 import datetime
 '''
 #sudo apt install python3-psutil -y;python3 -c "import psutil; print(psutil.cpu_percent(interval=1))"
@@ -13,18 +14,20 @@ class sys_stdout_file():
         self.files = files
 
     def write(self, obj):
-#        sys.__stdout__.write("TCL Output in Python stdout.OBJ invoke in the sys_class:\n")
-#        sys.__stdout__.write("DEBUG obj ="+ repr(obj)+ "\n")  # use repr() to get object representation
-#        sys.__stdout__.flush()
+        sys.__stdout__.write("TCL Output in Python stdout.OBJ invoke in the sys_class:\n")
+        sys.__stdout__.write("DEBUG obj ="+ repr(obj)+ "\n")  # use repr() to get object representation
+        sys.__stdout__.flush()
         for f in self.files:
             f.write(obj) # the content of write
-            f.flush()  # output to be visible immediately
+            f.flush()  # If you want the output to be visible immediately
 
     def flush(self):
         for f in self.files:
             f.flush()
 
 base_path = os.path.dirname(os.path.abspath(__file__))
+
+
 tcl_file = os.path.join(base_path, "tcl_test_0.tcl")
 tcl_test_result_path = os.path.join(base_path, "tcl_test_0_result.txt")
 
@@ -45,9 +48,9 @@ try:
     print("current time: {}\n".format(now.strftime("%Y-%m-%d %H:%M:%S")))
 
 
-    print("Invoke the Tcl scripts:\n")
+    print("The command is:\n")
     # invoke tclsh to execute TCL script
-    result = subprocess.run(["tclsh", tcl_file], capture_output=True, text=True,cwd=base_path)
+    result = subprocess.run(["tclsh", tcl_file], capture_output=True, text=True)
 
     print("TCL Output in Python OBJ return from TCL:\n")
     print("[DEBUG obj]", repr(result))
--- Python_Tcl/python_tcl_0/tcl_test_0.tcl
+++ Python_Tcl/python_tcl_0/tcl_test_0.tcl
@@ -1,51 +1,11 @@
 # test.tcl
+# Open file in append mode ("a")
+# set fp [open "tcl_0_result.txt" "a"]
 
-set scriptDir [file dirname [info script]]
-set filePath [file join $scriptDir "tcl_commands.txt"]
+puts "Running TCL script..."
 
-#puts "Script directory: $scriptDir"
-#puts "Target file path: $filePath"
+set result [exec uname -a]
+puts "System info 'uname -': $result"
 
-if {![file exists $filePath]} {
-    puts "❌ File not found: $filePath"
-    exit 1
-}
-
-
-puts "Open TCL file..."
-set fp [open $filePath r]
-set content [split [string map {"\r" ""} [read $fp]] "\n"] 
-close $fp
-
-#string map {key1 value1 key2 value2 ...} string
-
-#puts "Running TCL script list:\n"
-#foreach line $content {
-#    puts "Command line: $line"
-#}
-
-puts "\nRunning system commands..."
-foreach line $content {
-    if {$line ne ""} {
-        catch {
-            #set result [eval exec $line]
-            #puts "Command: $line"
-            #puts "Result:\n$result\n"
-            #set result [exec $line]
-            set result [eval exec $line]
-            puts "Line: $line \n': $result"
-
-        } errMsg
-        # errmsg is a special variable set by catch
-        if {[info exists errMsg] && $errMsg ne ""} {
-            puts "⚠️ Error executing '$line': $errMsg\n"
-        }
-    }
-}
-
-puts "✅ Tcl finished successfully."
-
-
-
-
-
+set result [exec cat /proc/stat]
+puts "CPU stats 'cat /proc/stat':\n$result"
